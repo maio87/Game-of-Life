988d9dcbed83309371dcdd5ab3d6d5f617589dff
Danilo Maiolatesi <maio@maio-laptop.(none)>
2011-09-24 02:43:59 +0200

    aggiunto: miglioramento grafica, espansione scala+ottave carica/salva configurazione

--- src/GOFrame.java
+++ src/GOFrame.java
@@ -1,29 +1,39 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Shape;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
+import java.awt.geom.Ellipse2D;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-
+import org.jfugue.Player;
+import org.jfugue.Instrument;
 class to_save implements Serializable{
 	
 	/**
@@ -56,11 +66,18 @@ public class GOFrame extends JFrame
 	
 	
 	Thread t;
-	JButton bplay,breset,bnext,bsave,bopen;
-	JButton[][] mat;
+	JButton bplay,breset,bnext,bsave,bopen,bcolor;
+	RoundButton[][] mat;
+	JColorChooser tcc = new JColorChooser();
+	Instrument ins = new Instrument((byte) 0);
+	JComboBox instrlist;
+	String[] ins_list = new String[127]; 
+	//petList.setSelectedIndex(4);
+
+
 	boolean[][] old_mat;
 	JPanel panel,p,pt;
-	final int dim = 30;
+	final int dim = 12;
 	
 	JLabel task = new JLabel();
 	ImageIcon implay = new ImageIcon("image/media-playback-start.png"); 
@@ -76,24 +93,41 @@ public class GOFrame extends JFrame
 	static final int RPS_MIN = 50;
 	static final int RPS_MAX = 500;
 	static final int RPS_INIT = 150;    //initial frames per second
+	
+	private Color buttoncolor  = new Color(00, 255, 0);
+	static final Color buttonbkg  = new Color(2,2,2);
+	static final Color windbkg  = new Color(2,2,2);
 
 	JSlider sleep;
 	JCheckBox toroButton;
+	JFileChooser choose;
 
+	
+	
 	public GOFrame()
 	{
+		for(int i=0;i<127;i++){
+			ins_list[i]=ins.getInstrumentName();
+			ins.setInstrument((byte) (i+1));
+		}
 		
+		System.out.println("#ins "+ins_list.length);
+		instrlist = new JComboBox(ins_list);
+		instrlist.setSelectedIndex(0);
+
+		System.out.println("selected "+instrlist.getSelectedItem());
 		setLayout(new BorderLayout());
 		setTitle("Game Of Life");
-		setSize(800,600);
+		setSize(600,650);
 		centerFrame();
-		
 		panel = new JPanel();
-		panel.setLayout( new GridLayout(dim,dim) );
+		GridLayout gpanel = new GridLayout(dim,dim);
+		panel.setLayout( gpanel );
+		panel.setBackground(windbkg);
 		p = new JPanel();
 		p.setLayout(new GridLayout(1,5));
-		pt = new JPanel(new BorderLayout());
-		mat = new JButton[dim][dim];
+		pt = new JPanel(new GridLayout(3,5));
+		mat = new RoundButton[dim][dim];
 		
 		 toroButton = new JCheckBox("toroidale");
 		
@@ -106,19 +140,24 @@ public class GOFrame extends JFrame
 		sleep.setMinorTickSpacing(1);
 		sleep.setPaintTicks(true);
 		sleep.setPaintLabels(true);
-		
+		Border insideBorder = BorderFactory.createRaisedBevelBorder();
+
+		Border outsideBorder = BorderFactory.createEtchedBorder(EtchedBorder.RAISED,buttonbkg,Color.WHITE);
+		Border border = BorderFactory.createCompoundBorder(outsideBorder, insideBorder);
 		for(int i=0; i<dim;i++)
 			for(int j=0;j<dim;j++)
 			{
-				mat[i][j] = new JButton();
+				mat[i][j] = new RoundButton("",new Dimension(20,20));
 				mat[i][j].setSelected(false);
-				mat[i][j].setBackground(Color.WHITE);
+				mat[i][j].setBackground(buttonbkg);
 				mat[i][j].addActionListener( new BAction() );
+				//mat[i][j].setBorder(border);
+				
 				panel.add(mat[i][j]);
 			}
 		
 		bplay = new JButton(implay);
-		bplay.setBackground(Color.WHITE);
+		bplay.setBackground(buttonbkg);
 		bplay.addActionListener(new ActionListener(){
 		
 			public void actionPerformed(ActionEvent e)
@@ -146,7 +185,7 @@ public class GOFrame extends JFrame
 		p.add(bplay);
 		
 		bnext = new JButton(imnext);
-		bnext.setBackground(Color.WHITE);
+		bnext.setBackground(buttonbkg);
 		bnext.addActionListener(new ActionListener(){
 
 			public void actionPerformed(ActionEvent e)
@@ -160,7 +199,7 @@ public class GOFrame extends JFrame
 		p.add(bnext);
 		
 		breset = new JButton(imreset);
-		breset.setBackground(Color.WHITE);
+		breset.setBackground(buttonbkg);
 		breset.addActionListener(new ActionListener(){
 
 			public void actionPerformed(ActionEvent e)
@@ -172,7 +211,7 @@ public class GOFrame extends JFrame
 
 						mat[i][j].setSelected(false);
 						periodo =0;
-						mat[i][j].setBackground(Color.WHITE);
+						mat[i][j].setBackground(buttonbkg);
 						vive=0;
 						task.setText(" vive: "+vive+"     morte: "+(dim*dim-vive)+"     periodo: "+periodo);
 					}
@@ -181,15 +220,56 @@ public class GOFrame extends JFrame
 		});
 		
 		p.add(breset);
+
+		bcolor = new JButton("Choose Color");
+		bcolor.setBackground(Color.LIGHT_GRAY);
+		bcolor.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent e)
+			{
+				//1. Create the frame.
+				JFrame frame = new JFrame("Choose Color");
+
+				//2. Optional: What happens when the frame closes?
+				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+				//3. Create components and put them in the frame.
+				//...create emptyLabel...
+				frame.getContentPane().add(tcc, BorderLayout.CENTER);
+				
+				//4. Size the frame.
+				frame.pack();
+
+				//5. Show it.
+				frame.setVisible(true);
+
+			}
+			
+		});
+		
+		tcc.getSelectionModel().addChangeListener(new ChangeListener(){
+
+			public void stateChanged(ChangeEvent e) {
+			    buttoncolor = tcc.getColor();
+			}
+			
+		});
 		
 
 		task.setText(" vive: "+vive+"     morte: "+(dim*dim-vive)+"     periodo: "+periodo);
 		//task.setAlignmentY(Component.LEFT_ALIGNMENT);
+		
 		pt.add(task);
+		pt.add(new JLabel("   "));
 		pt.add(toroButton,BorderLayout.PAGE_END);
+		
 		pt.add(sleep,BorderLayout.EAST);
+		pt.add(instrlist);
+
+		pt.add(bcolor);
+		
 		bsave = new JButton(imsave);
-		bsave.setBackground(Color.WHITE);
+		bsave.setBackground(buttonbkg);
 	//	bsave.setPreferredSize(new Dimension(45,45));
 		
 		
@@ -198,28 +278,40 @@ public class GOFrame extends JFrame
 
 			public void actionPerformed(ActionEvent e)
 			{
-				System.out.print("SAVING...");
 			    FileOutputStream fos = null;
 			    ObjectOutputStream out = null;
-			    String filename = new String("out.txt");
-			    try
-			         {
-			           fos = new FileOutputStream(filename);
-			           out = new ObjectOutputStream(fos);
-			           out.writeObject(new to_save(mat,dim));
-			           out.close();
-			         }
-			         catch(IOException ex)
-			         {
-			          ex.printStackTrace();
-			         }				
+			    String nome_file = "";
+			    choose = new JFileChooser();
+			    choose.setCurrentDirectory(new File("./"));
+			    int n = choose.showSaveDialog(((JButton)e.getSource()).getParent());
+			    if(n == JFileChooser.APPROVE_OPTION)
+			    {
 
-				System.out.println("DONE");
+					System.out.print("SAVING...");
+			    	nome_file = choose.getSelectedFile().toString();
+				    String filename = new String(nome_file);
+				    try
+				         {
+				           fos = new FileOutputStream(filename);
+				           out = new ObjectOutputStream(fos);
+				           out.writeObject(new to_save(mat,dim));
+				           out.close();
+				         }
+				         catch(IOException ex)
+				         {
+				          ex.printStackTrace();
+				         }				
+
+					System.out.println("DONE");
+			    }
+			    
+			    
+
 			}
 			
 		});
 		bopen = new JButton(imopen);
-		bopen.setBackground(Color.WHITE);
+		bopen.setBackground(buttonbkg);
 	//	bopen.setPreferredSize(new Dimension(45,45));
 		
 		bopen.addActionListener(new ActionListener(){
@@ -230,46 +322,59 @@ public class GOFrame extends JFrame
 				   FileInputStream fis = null;
 				   ObjectInputStream in = null;
 				   to_save app = null;
-				    String filename = new String("out.txt");
-				   try
-				   {
-				     fis = new FileInputStream(filename);
-				     in = new ObjectInputStream(fis);
-				     app = (to_save)in.readObject();
-				     in.close();
-				     boolean[][]saved_mat=app.getSave_mat();
-				     vive = 0;
-				     for(int i=0; i<dim; i++)
-							for(int j=0; j<dim; j++)
-							{
-								  mat[i][j].setSelected(saved_mat[i][j]);
+				   
 
-									if (mat[i][j].isSelected()){
-										mat[i][j].setBackground(Color.RED);
-										vive++;
-									}
-									else
+				    choose = new JFileChooser();
+
+				    choose.setCurrentDirectory(new File("./"));
+				   int n = choose.showOpenDialog(((JButton)e.getSource()).getParent());
+				    if(n == JFileChooser.APPROVE_OPTION)
+				    {
+
+					    String filename = choose.getSelectedFile().toString() ;
+					    try
+						   {
+						     fis = new FileInputStream(filename);
+						     in = new ObjectInputStream(fis);
+						     app = (to_save)in.readObject();
+						     in.close();
+						     boolean[][]saved_mat=app.getSave_mat();
+						     vive = 0;
+						     for(int i=0; i<dim; i++)
+									for(int j=0; j<dim; j++)
 									{
-										mat[i][j].setBackground(Color.WHITE);
-									}
+										  mat[i][j].setSelected(saved_mat[i][j]);
 
-							}
-					
-				     
-				 	task.setText(" vive: "+vive+"     morte: "+(dim*dim-vive)+"     periodo: "+periodo);
-					 
-				   }
-				   catch(IOException ex)
-				   {
-				     ex.printStackTrace();
-				   }
-				   catch(ClassNotFoundException ex)
-				   {
-				     ex.printStackTrace();
+											if (mat[i][j].isSelected()){
+												mat[i][j].setBackground(buttoncolor);
+												vive++;
+											}
+											else
+											{
+												mat[i][j].setBackground(buttonbkg);
+											}
+
+									}
+							
+						     
+						 	task.setText(" vive: "+vive+"     morte: "+(dim*dim-vive)+"     periodo: "+periodo);
+							 
+						   }
+						   catch(IOException ex)
+						   {
+						     ex.printStackTrace();
+						   }
+						   catch(ClassNotFoundException ex)
+						   {
+						     ex.printStackTrace();
+						   }
+						   
+						   
+						System.out.println("DONE");
+				    	
 				   }
 				   
-				   
-				System.out.println("DONE");
+				  
 			}
 			
 		});
@@ -294,7 +399,7 @@ public class GOFrame extends JFrame
 
 				((JButton)e.getSource()).setSelected(true);
 
-				((JButton)e.getSource()).setBackground(Color.RED);
+				((JButton)e.getSource()).setBackground(buttoncolor);
 				vive++;
 				task.setText(" vive: "+vive+"     morte: "+(dim*dim-vive)+"     periodo: "+periodo);
 			}
@@ -302,7 +407,7 @@ public class GOFrame extends JFrame
 			{
 				((JButton)e.getSource()).setSelected(false);
 
-				((JButton)e.getSource()).setBackground(Color.WHITE);
+				((JButton)e.getSource()).setBackground(buttonbkg);
 				vive--;
 				task.setText(" vive: "+vive+"     morte: "+(dim*dim-vive)+"     periodo: "+periodo);
 			}
@@ -351,6 +456,17 @@ public class GOFrame extends JFrame
 		
 	}
 	
+	public boolean[][] getMat() {
+		boolean[][] ret_mat= new boolean [this.dim][this.dim];
+		for(int i=0; i<dim; i++)
+			for(int j=0; j<dim; j++)
+			{
+				ret_mat[i][j] = mat[i][j].isSelected();
+				
+			}
+		return ret_mat;
+	}
+	
 	private void prossimo_periodo(){
 		old_mat = new boolean[dim][dim];
 		for(int i=0; i<dim; i++)
@@ -367,22 +483,57 @@ public class GOFrame extends JFrame
 				
 				if ( !mat[i][j].isSelected() && (Adiacenze(old_mat,i,j) == 3) ){
 					mat[i][j].setSelected(true);
-					mat[i][j].setBackground(Color.RED);
+					mat[i][j].setBackground(buttoncolor);
 					vive++;
 				}
 				else if ( mat[i][j].isSelected() &&( Adiacenze(old_mat,i,j)<2 || Adiacenze(old_mat,i,j)>3 ) )
 				{
 
 					mat[i][j].setSelected(false);
-					mat[i][j].setBackground(Color.WHITE);
+					mat[i][j].setBackground(buttonbkg);
 					vive--;
 				}
 			}
 		}
 		periodo ++ ;
 		task.setText(" vive: "+vive+"     morte: "+(dim*dim-vive)+"     periodo: "+periodo);
+	
+	
+		int len;
 		
+		String strplay = "I["+ins_list[instrlist.getSelectedIndex()]+"] T[Presto] ";
+		String startstr = strplay;
+		for(int i=0; i<dim; i++){
+			for(int j=0; j<dim; j++)
+			{
+				if(mat[i][j].isSelected() && !strplay.contains(note[i%note.length])){
 
+				//	System.out.println(strplay+" <-strp  "+note[i%7]);
+					strplay= strplay + note[i%7]+j%10+ "q ";
+					
+				}
+			}
+			if((len=strplay.length())!=0){
+			 
+			//System.out.println("gen: "+strplay);
+			strplay=strplay.substring(0,len-1);
+
+			//System.out.println("gen2: "+strplay);
+			strplay += " ";
+			}
+
+		}
+
+		if(!strplay.equals(startstr)){
+			len=strplay.length();
+			strplay=strplay.substring(0,len-1);
+			System.out.println("genf: "+strplay+"$");
+			PlaySound p = new PlaySound(strplay);
+			Thread t = new Thread(p);
+			
+			t.start();
+			
+		}
 		
 	}
 	
@@ -392,7 +543,7 @@ public class GOFrame extends JFrame
 		public void run()
 		{
 			while(true){
-				prossimo_periodo();			
+				prossimo_periodo();
 				try {
 					Thread.sleep(sleep.getValue());
 				} catch (InterruptedException e) {
@@ -402,4 +553,75 @@ public class GOFrame extends JFrame
 		}
 	}
 	
+
+	Player player = new Player();
+	
+	String[] note = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
+
+	private class PlaySound implements Runnable
+	{
+		String toPlay;
+		PlaySound(String str){
+			this.toPlay = str;
+		}
+		public void run()
+		{
+				player.play(this.toPlay);
+		}
+	}
+
+	public class RoundButton extends JButton {
+		  public RoundButton(String label, Dimension d) {
+		    super(label);
+
+		// These statements enlarge the button so that it 
+		// becomes a circle rather than an oval.
+		    Dimension size = d;
+		    size.width = size.height = Math.max(size.width, 
+		      size.height);
+		    setPreferredSize(size);
+
+		// This call causes the JButton not to paint 
+		   // the background.
+		// This allows us to paint a round background.
+		    setContentAreaFilled(false);
+		  }
+
+		// Paint the round background and label.
+		  protected void paintComponent(Graphics g) {
+		    if (getModel().isArmed()) {
+		// You might want to make the highlight color 
+		   // a property of the RoundButton class.
+		      g.setColor(Color.lightGray);
+		    } else {
+		      g.setColor(getBackground());
+		    }
+		    g.fillOval(0, 0, getSize().width-1, 
+		      getSize().height-1);
+
+		// This call will paint the label and the 
+		   // focus rectangle.
+		    super.paintComponent(g);
+		  }
+
+		// Paint the border of the button using a simple stroke.
+		  protected void paintBorder(Graphics g) {
+		    g.setColor(getForeground());
+		    g.drawOval(0, 0, getSize().width-1, 
+		      getSize().height-1);
+		  }
+
+		// Hit detection.
+		  Shape shape;
+		  public boolean contains(int x, int y) {
+		// If the button has changed size, 
+		   // make a new shape object.
+		    if (shape == null || 
+		      !shape.getBounds().equals(getBounds())) {
+		      shape = new Ellipse2D.Float(0, 0, 
+		        getWidth(), getHeight());
+		    }
+		    return shape.contains(x, y);
+		  }
+	}
 }
--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,14 +1,21 @@
 import javax.swing.JFrame;
 
+import org.jfugue.Player;
+
 
 public class GameOfLife
 {
 	public static void main(String[] args)
 	{
 		
-		GOFrame gof = new GOFrame();
-		gof.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gof.setVisible(true);
+		GOFrame[] gof = new GOFrame[3];
+		for(int i = 0 ; i<3 ; i++){
+			gof[i] = new GOFrame();
+			gof[i].setVisible(true);
+			gof[i].setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+		}
+		System.out.println("end main");
 	}
-
+	
 }
